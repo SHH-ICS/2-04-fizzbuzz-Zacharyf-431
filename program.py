6773ad45de362ff08802dd99c6b3777782b2fa29
Zacharyf-431 <zachary.fowler1@stu.ocsb.ca>
2024-10-15 22:33:51 +0000

    done

--- program.py
+++ program.py
@@ -6,8 +6,12 @@
 # For multiples of 15, output   FizzBuzz
 # End at 32
 
-result = ""
-for myNumber in range(32):
-  result = str(result) + str(myNumber) + "\n"
-
-print(result)
+for myNumber in range(1, 33):
+  if myNumber % 3 == 0 and myNumber % 5 == 0:
+    print ("Fizzbuzz")
+  elif myNumber % 3 == 0:
+    print ("Fizz")
+  elif myNumber % 5 == 0:
+    print ("Buzz")
+  else:
+    print (myNumber)
